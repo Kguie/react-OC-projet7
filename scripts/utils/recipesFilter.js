df65a2c123854bd82e2b7fc65a53d0d6f1cf31a3
Kévin Guieba <107431452+Kguie@users.noreply.github.com>
2024-03-15 12:55:10 -0400

    Ajout du texte explicatif en cas de résultat de recherche nul

--- scripts/utils/recipesFilter.js
+++ scripts/utils/recipesFilter.js
@@ -1,6 +1,6 @@
 async function handleSearch(recipes) {
-  let searchList = getRecipesSearchArray();
-  let recipesList = recipes || (await getRecipes());
+  let searchList = await getRecipesSearchArray();
+  let recipesList = recipes || (await getRecipes()) || [];
   if (searchList.length) {
     searchList.forEach((element) => {
       if (recipesList.length) {
@@ -12,17 +12,33 @@ async function handleSearch(recipes) {
   }
   await handleLists(recipesList);
   displayRecipes(recipesList);
-  launchRecipesCount();
+  launchRecipesCount(recipesList, searchList);
 }
 
-function launchRecipesCount() {
-  const $recipesList = document.querySelectorAll(".recipe-card");
+function launchRecipesCount(recipeList, searchList) {
+  // const $recipesList = document.querySelectorAll(".recipe-card");
   const $counter = document.querySelector(
     ".search-section__choices-line__recipes-counter"
   );
-  $counter.textContent = `${$recipesList.length} recette${
-    $recipesList.length > 1 ? "s" : ""
+  $counter.textContent = `${recipeList?.length || 0} recette${
+    (recipeList?.length || 0) > 1 ? "s" : ""
   }`;
+  displayNoRecipesFounded(recipeList, searchList);
+}
+
+function displayNoRecipesFounded(recipeList, searchList) {
+  const $disclaimer = document.querySelector(".disclaimer__empty");
+  const $disclaimerKey = document.querySelector(".disclaimer__empty__key");
+
+  const mainElement = searchList?.find((element) => element.main);
+
+  if (mainElement && (recipeList.length || 0) === 0) {
+    $disclaimer.style.display = "block";
+    $disclaimerKey.textContent = mainElement.main;
+  } else {
+    $disclaimer.style.display = "none";
+    $disclaimerKey.textContent = "";
+  }
 }
 
 /**
